f2767f5c3451054040e770292f700d37b9c13d2e
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-10-30 23:50:13 +0100

    add configuration to permitall to home page without authentication

---
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
@@ -0,0 +1,21 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest
+public class IndexControllerIT extends BaseIT{
+
+
+    @Test
+    void getIndexSlash() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+
+}

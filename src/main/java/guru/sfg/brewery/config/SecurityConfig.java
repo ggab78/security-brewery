f85b8c8dfb8af0d0566af595285b50e6e834e826
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-10-31 17:07:21 +0100

    configure different password encoders with delegating password encoder

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -11,10 +11,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.*;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -27,7 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //return NoOpPasswordEncoder.getInstance();
         //return new LdapShaPasswordEncoder();
         //return new StandardPasswordEncoder();
-        return new BCryptPasswordEncoder();
+        //return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
@@ -55,26 +54,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("gab")
-                .password("$2a$10$Up23Vp4h6dkBD.bsqhrEx.yfOyMT62iWv/E7ymL4hyza07ZfGK8ri")//for bcrypt
-//                .password("52dd91af279225298fd1e25ed368185ae3cdc8d93cd67e2f1497c3b74f07d78abf38931c1a2cd5e8")//for sha256
-                //.password("{SSHA}1CIB+3H5UKy1I44hZLnySzoUNCJbcgDduzMbPw==")//for ldap
-                //.password("hugo")//for noop configuration
+                .password("{bcrypt}$2a$10$Up23Vp4h6dkBD.bsqhrEx.yfOyMT62iWv/E7ymL4hyza07ZfGK8ri")//for bcrypt
+//              .password("{sha256}52dd91af279225298fd1e25ed368185ae3cdc8d93cd67e2f1497c3b74f07d78abf38931c1a2cd5e8")//for sha256
+//                .password("{ldap}{SSHA}1CIB+3H5UKy1I44hZLnySzoUNCJbcgDduzMbPw==")//for ldap
+//                .password("{noop}hugo")//for noop configuration
                 .roles("ADMIN")
 
                 .and()
                 .withUser("user")
-                .password("$2a$10$X0IyFcUlKSMfO0Lk7BzuVOkHUj99/X6LebypYcxYOZ0xrrIg52Ki6")//for bcrypt
-//                .password("e8d5544810407492e04c3fbbd8a2619645a8547a8ef1bd8a325042b8521784c71115364a4ef85e43")//for sha256
-                //.password("{SSHA}UT0nOqMAtH1ah3ZTrNNZ+wRsFtyRfc7pWFMfUw==")//for ldap
-                //.password("password") //for noop configuration
+                .password("{bcrypt}$2a$10$X0IyFcUlKSMfO0Lk7BzuVOkHUj99/X6LebypYcxYOZ0xrrIg52Ki6")//for bcrypt
+//                .password("{sha256}e8d5544810407492e04c3fbbd8a2619645a8547a8ef1bd8a325042b8521784c71115364a4ef85e43")//for sha256
+//                .password("{ldap}{SSHA}UT0nOqMAtH1ah3ZTrNNZ+wRsFtyRfc7pWFMfUw==")//for ldap
+//                .password("{noop}password") //for noop configuration
                 .roles("USER")
 
                 .and()
                 .withUser("hugo")
-                .password("$2a$10$x.WxWcJvqkId5e6w5w0FS.Xj8W0FOpqlo7RU5oouxLrjpiJXycGjK")//for bcrypt
-//                .password("6ab5d90bf21e491b1369b97aa277dc3b2f6e86bfb17bb2350cba08e323c963572d63755802cf3b77")//for sha256
-                //.password("{SSHA}qyEVZxw2Vk5761xxtMhgrUd9kBcWvG4twQPI6A==")//for ldap
-                //.password("boss")//for noop configuration
+                .password("{bcrypt}$2a$10$x.WxWcJvqkId5e6w5w0FS.Xj8W0FOpqlo7RU5oouxLrjpiJXycGjK")//for bcrypt
+//                .password("{sha256}6ab5d90bf21e491b1369b97aa277dc3b2f6e86bfb17bb2350cba08e323c963572d63755802cf3b77")//for sha256
+//                .password("{ldap}{SSHA}qyEVZxw2Vk5761xxtMhgrUd9kBcWvG4twQPI6A==")//for ldap
+//                .password("{noop}boss")//for noop configuration
                 .roles("CUSTOMER");
 
     }

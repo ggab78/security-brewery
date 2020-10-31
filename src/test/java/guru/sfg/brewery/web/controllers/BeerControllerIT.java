05a642bc3a7710cb6db44abc7f57d636d49b5a7e
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-10-31 11:35:03 +0100

    configure inMemoryUserAuthentication

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -22,6 +22,14 @@ public class BeerControllerIT extends BaseIT{
                 .andExpect(model().attributeExists("beer"));
     }
 
+    @Test
+    void initCreateForm() throws Exception{
+        mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
 
     @Test
     void findBeersWithHttpBasic() throws Exception{

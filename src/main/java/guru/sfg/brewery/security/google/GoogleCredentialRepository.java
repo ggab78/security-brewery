14343a88e0bc21a148e9f11f2d1757dfb6f0f947
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-11-20 21:52:19 +0100

    config google secret persistence

---
+++ src/main/java/guru/sfg/brewery/security/google/GoogleCredentialRepository.java
@@ -0,0 +1,36 @@
+package guru.sfg.brewery.security.google;
+
+import com.warrenstrange.googleauth.ICredentialRepository;
+import guru.sfg.brewery.domain.security.User;
+import guru.sfg.brewery.repositories.security.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class GoogleCredentialRepository implements ICredentialRepository {
+
+
+    private final UserRepository userRepository;
+
+    @Override
+    public String getSecretKey(String userName) {
+        User user= userRepository.findByUsername(userName).orElseThrow();
+        return user.getGoogleSecret();
+
+    }
+
+    @Override
+    public void saveUserCredentials(String userName, String secretKey, int validationCode, List<Integer> scratchCodes) {
+
+        User user= userRepository.findByUsername(userName).orElseThrow();
+        user.setGoogleSecret(secretKey);
+        user.setUserGoogle2fa(true);
+
+        userRepository.save(user);
+    }
+}
